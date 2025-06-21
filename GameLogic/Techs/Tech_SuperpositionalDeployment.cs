9763363f824220ab0e5a5c21863f33df252fc8db
Autoquark <james.a.badcoe@gmail.com>
2025-06-21 10:41:25 +0100

    Superpositional Deployment tech

--- GameLogic/Techs/Tech_SuperpositionalDeployment.cs
+++ GameLogic/Techs/Tech_SuperpositionalDeployment.cs
@@ -1,6 +1,38 @@
+using SpaceWarDiscordApp.Database;
+using SpaceWarDiscordApp.GameLogic.Operations;
+
 namespace SpaceWarDiscordApp.GameLogic.Techs;
 
-public class Tech_SuperpositionalDeployment
+public class Tech_SuperpositionalDeployment : Tech
 {
-    
+    public Tech_SuperpositionalDeployment() : base("superpositionalDeployment",
+        "Superpositional Deployment",
+        "Free Action, Once per turn: Move any number of forces from one planet you control to an adjacent planet you control.",
+        "It's not so much travelling as always having been there.")
+    {
+        HasSimpleAction = true;
+        SimpleActionType = ActionType.Free;
+        SimpleActionIsOncePerTurn = true;
+        _movementFlowHandler = new SuperpositionalDeployment_MovementFlowHandler(this);
+        AdditionalInteractionHandlers = [_movementFlowHandler];
+    }
+
+    private readonly SuperpositionalDeployment_MovementFlowHandler _movementFlowHandler;
+
+    protected override bool IsSimpleActionAvailable(Game game, GamePlayer player) 
+        => base.IsSimpleActionAvailable(game, player) && game.Hexes.WhereOwnedBy(player).Any(x => BoardUtils.GetNeighbouringHexes(game, x).WhereOwnedBy(player).Any());
+
+    public override Task<TBuilder> UseTechActionAsync<TBuilder>(TBuilder builder, Game game, GamePlayer player)
+        => _movementFlowHandler.BeginPlanningMoveAsync(builder, game, player);
+}
+
+public class SuperpositionalDeployment_MovementFlowHandler : MovementFlowHandler<Tech_SuperpositionalDeployment>
+{
+    public SuperpositionalDeployment_MovementFlowHandler(Tech_SuperpositionalDeployment tech) : base(tech.DisplayName)
+    {
+        AllowManyToOne = false;
+        MarkUsedTechId = tech.Id;
+        ActionType = tech.SimpleActionType;
+        DestinationRestriction = MoveDestinationRestriction.MustAlreadyControl;
+    }
 }
