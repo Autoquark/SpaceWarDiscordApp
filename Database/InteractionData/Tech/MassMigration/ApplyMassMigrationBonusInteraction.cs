576bef96f2f0700d0b270b90ef42e8d3c04989dd
Autoquark <james.a.badcoe@gmail.com>
2025-09-07 22:22:42 +0100

    Missing FirestoreData attribute

--- Database/InteractionData/Tech/MassMigration/ApplyMassMigrationBonusInteraction.cs
+++ Database/InteractionData/Tech/MassMigration/ApplyMassMigrationBonusInteraction.cs
@@ -1,7 +1,9 @@
+using Google.Cloud.Firestore;
 using SpaceWarDiscordApp.Database.GameEvents;
 
 namespace SpaceWarDiscordApp.Database.InteractionData.Tech.MassMigration;
 
+[FirestoreData]
 public class ApplyMassMigrationBonusInteraction : EventModifyingInteractionData<GameEvent_PreMove>
 {
     
