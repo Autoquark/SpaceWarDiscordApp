53ef0b5a58a2dcb55806234ecb897845f30bb168
Autoquark <james.a.badcoe@gmail.com>
2025-10-11 22:50:43 +0100

    Flag optimisation fix

--- Database/InteractionData/Tech/FlagOptimisation/ApplyFlagOptimisationBonusInteraction.cs
+++ Database/InteractionData/Tech/FlagOptimisation/ApplyFlagOptimisationBonusInteraction.cs
@@ -1,9 +1,14 @@
-﻿using SpaceWarDiscordApp.Database.GameEvents;
+﻿using Google.Cloud.Firestore;
+using SpaceWarDiscordApp.Database.GameEvents;
 
 namespace SpaceWarDiscordApp.Database.InteractionData.Tech.FlagOptimisation;
 
+[FirestoreData]
 public class ApplyFlagOptimisationBonusInteraction : TriggeredEffectInteractionData
 {
+    [FirestoreProperty]
     public required GameEvent_PreMove Event { get; set; }
+    
+    [FirestoreProperty]
     public required bool IsAttacker { get; set; }
 }
