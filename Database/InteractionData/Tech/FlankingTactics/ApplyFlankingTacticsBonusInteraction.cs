4b87e772e71a437d5074c1c935c411f8b3e2ce3a
Autoquark <james.a.badcoe@gmail.com>
2025-11-12 23:15:06 +0000

    Fix flanking tactics

--- Database/InteractionData/Tech/FlankingTactics/ApplyFlankingTacticsBonusInteraction.cs
+++ Database/InteractionData/Tech/FlankingTactics/ApplyFlankingTacticsBonusInteraction.cs
@@ -1,7 +1,9 @@
+using Google.Cloud.Firestore;
 using SpaceWarDiscordApp.Database.GameEvents;
 
 namespace SpaceWarDiscordApp.Database.InteractionData.Tech.FlankingTactics;
 
+[FirestoreData]
 public class ApplyFlankingTacticsBonusInteraction : EventModifyingInteractionData<GameEvent_PreMove>
 {
 }
