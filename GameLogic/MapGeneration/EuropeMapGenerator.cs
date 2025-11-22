ea67d0aa1293e573a92c42e6a316fe8b335c335c
jamespellis2 <jamespellis2@gmail.com>
2025-11-22 17:15:03 +0000

    Europe Map: Added hyperlanes.

--- GameLogic/MapGeneration/EuropeMapGenerator.cs
+++ GameLogic/MapGeneration/EuropeMapGenerator.cs
@@ -27,7 +27,9 @@ public class EuropeMapGenerator : BaseMapGenerator
         BoardHex star = new BoardHex() { Planet = new Planet() { Stars = 1, Production = 1, Science = 0, ForcesPresent = 1, } };
         List<BoardHex> ownedBoardHexes = new List<BoardHex>() { capital, science, people, star };
         BoardHex sea = new BoardHex();
-        BoardHex neutral = new BoardHex() { Planet = new Planet() { Stars = 1, Production = 1, Science = 0, ForcesPresent = 0, } };
+        BoardHex neutralStar = new BoardHex() { Planet = new Planet() { Stars = 1, Production = 1, Science = 0, ForcesPresent = 0, } };
+        BoardHex neutralScience = new BoardHex() { Planet = new Planet() { Stars = 0, Production = 1, Science = 1, ForcesPresent = 0, } };
+        BoardHex neutralPeople = new BoardHex() { Planet = new Planet() { Stars = 0, Production = 3, Science = 0, ForcesPresent = 0, } };
 
         var map = new List<BoardHex>();
 
@@ -37,14 +39,39 @@ public class EuropeMapGenerator : BaseMapGenerator
         map.Add(new BoardHex(sea) { Coordinates = new HexCoordinates(0, -4) });
         map.Add(new BoardHex(sea) { Coordinates = new HexCoordinates(0, -3) });
         map.Add(new BoardHex(sea) { Coordinates = new HexCoordinates(0, -2) });
-        map.Add(new BoardHex(neutral) { Coordinates = new HexCoordinates(0, -1) });
+        map.Add(new BoardHex() { Planet = new Planet() { Stars = 1, Production = 2, Science = 1, ForcesPresent = 0, }, Coordinates = new HexCoordinates(0, -1) });
         map.Add(new BoardHex(star) { Coordinates = new HexCoordinates(-1, -4) });
         map.Add(new BoardHex(science) { Coordinates = new HexCoordinates(-1, -3) });
         map.Add(new BoardHex(capital) { Coordinates = new HexCoordinates(-1, -2) });
         map.Add(new BoardHex(sea) { Coordinates = new HexCoordinates(-1, -1) });
         map.Add(new BoardHex(sea) { Coordinates = new HexCoordinates(-2, -3) });
         map.Add(new BoardHex(people) { Coordinates = new HexCoordinates(-2, -2) });
-        map.Add(new BoardHex(sea) { Coordinates = new HexCoordinates(-2, -1) });
+        map.Add(new BoardHex() { Coordinates = new HexCoordinates(-2, -4), HyperlaneConnections = [new HyperlaneConnection(HexDirection.NorthEast, HexDirection.South)] });
+        map.Add(new BoardHex() { Coordinates = new HexCoordinates(-1, -5), HyperlaneConnections = [new HyperlaneConnection(HexDirection.SouthWest, HexDirection.SouthEast)] });
+        map.Add(new BoardHex() { 
+            Coordinates = new HexCoordinates(0, -5), 
+            HyperlaneConnections = [
+                new HyperlaneConnection(HexDirection.SouthWest, HexDirection.SouthEast), 
+                new HyperlaneConnection(HexDirection.NorthWest, HexDirection.SouthEast)
+                ]
+        });
+        map.Add(new BoardHex() { Coordinates = new HexCoordinates(-3, -2), HyperlaneConnections = [new HyperlaneConnection(HexDirection.NorthEast, HexDirection.South)] });
+        map.Add(new BoardHex() { Coordinates = new HexCoordinates(-3, -1), HyperlaneConnections = [new HyperlaneConnection(HexDirection.North, HexDirection.South)] });
+        map.Add(new BoardHex() { 
+            Coordinates = new HexCoordinates(-3, 0), 
+            HyperlaneConnections = [
+                new HyperlaneConnection(HexDirection.North, HexDirection.South),
+                new HyperlaneConnection(HexDirection.NorthEast, HexDirection.South)
+                ]
+        });
+        map.Add(new BoardHex()
+        {
+            Coordinates = new HexCoordinates(-2, -1),
+            HyperlaneConnections = [
+                new HyperlaneConnection(HexDirection.North, HexDirection.SouthEast),
+                new HyperlaneConnection(HexDirection.North, HexDirection.SouthWest)
+                ]
+        });
 
         // France
         game.Players[1].PlayerColour = PlayerColour.Cyan;
@@ -55,11 +82,23 @@ public class EuropeMapGenerator : BaseMapGenerator
         map.Add(new BoardHex(sea) { Coordinates = new HexCoordinates(-1, 3) });
         map.Add(new BoardHex(science) { Coordinates = new HexCoordinates(-2, 0) });
         map.Add(new BoardHex(people) { Coordinates = new HexCoordinates(-2, 1) });
-        map.Add(new BoardHex(neutral) { Coordinates = new HexCoordinates(-2, 2) });
+        map.Add(new BoardHex(neutralPeople) { Coordinates = new HexCoordinates(-2, 2) });
         map.Add(new BoardHex(sea) { Coordinates = new HexCoordinates(-2, 3) });
-        map.Add(new BoardHex(neutral) { Coordinates = new HexCoordinates(-2, 4) });
+        map.Add(new BoardHex(neutralStar) { Coordinates = new HexCoordinates(-2, 4) });
         map.Add(new BoardHex(sea) { Coordinates = new HexCoordinates(-3, 1) });
-        map.Add(new BoardHex(neutral) { Coordinates = new HexCoordinates(-3, 3) });
+        map.Add(new BoardHex(neutralStar) { Coordinates = new HexCoordinates(-3, 3) });
+        map.Add(new BoardHex()
+        {
+            Coordinates = new HexCoordinates(-3, 2),
+            HyperlaneConnections = [
+                new HyperlaneConnection(HexDirection.North, HexDirection.SouthEast),
+                new HyperlaneConnection(HexDirection.North, HexDirection.South),
+                new HyperlaneConnection(HexDirection.North, HexDirection.SouthWest),
+                ]
+        });
+        map.Add(new BoardHex() { Coordinates = new HexCoordinates(-4, 3), HyperlaneConnections = [new HyperlaneConnection(HexDirection.NorthEast, HexDirection.South)] });
+        map.Add(new BoardHex() { Coordinates = new HexCoordinates(-4, 4), HyperlaneConnections = [new HyperlaneConnection(HexDirection.North, HexDirection.SouthEast)] });
+        map.Add(new BoardHex() { Coordinates = new HexCoordinates(-3, 4), HyperlaneConnections = [new HyperlaneConnection(HexDirection.NorthWest, HexDirection.NorthEast)] });
 
         // Germany
         if (playerCount > 2)
@@ -79,15 +118,24 @@ public class EuropeMapGenerator : BaseMapGenerator
         }
         SetPlanetOwnership(ownedBoardHexes, game.Players.ElementAtOrDefault(3)?.GamePlayerId ?? -1);
         map.Add(new BoardHex(sea) { Coordinates = new HexCoordinates(1, -5) });
-        map.Add(new BoardHex(neutral) { Coordinates = new HexCoordinates(1, -4) });
-        map.Add(new BoardHex(neutral) { Coordinates = new HexCoordinates(2, -5) });
-        map.Add(new BoardHex(neutral) { Coordinates = new HexCoordinates(2, -4) });
+        map.Add(new BoardHex(neutralStar) { Coordinates = new HexCoordinates(1, -4) });
+        map.Add(new BoardHex(neutralStar) { Coordinates = new HexCoordinates(2, -5) });
+        map.Add(new BoardHex(neutralScience) { Coordinates = new HexCoordinates(2, -4) });
         map.Add(new BoardHex(sea) { Coordinates = new HexCoordinates(2, -3) });
         map.Add(new BoardHex(sea) { Coordinates = new HexCoordinates(3, -6) });
         map.Add(new BoardHex(star) { Coordinates = new HexCoordinates(3, -5) });
         map.Add(new BoardHex(capital) { Coordinates = new HexCoordinates(3, -4) });
         map.Add(new BoardHex(science) { Coordinates = new HexCoordinates(3, -3) });
         map.Add(new BoardHex(people) { Coordinates = new HexCoordinates(3, -2) });
+        map.Add(new BoardHex() { Coordinates = new HexCoordinates(2, -6), HyperlaneConnections = [new HyperlaneConnection(HexDirection.SouthWest, HexDirection.SouthEast)] });
+        map.Add(new BoardHex() 
+        { 
+            Coordinates = new HexCoordinates(1, -3), 
+            HyperlaneConnections = [
+            new HyperlaneConnection(HexDirection.NorthWest, HexDirection.SouthEast),
+            new HyperlaneConnection(HexDirection.North, HexDirection.South)
+            ]
+        });
 
         // Austria-Hungary
         if (playerCount > 4)
@@ -99,7 +147,7 @@ public class EuropeMapGenerator : BaseMapGenerator
         map.Add(new BoardHex(people) { Coordinates = new HexCoordinates(2, -1) });
         map.Add(new BoardHex(capital) { Coordinates = new HexCoordinates(2, 0) });
         map.Add(new BoardHex(science) { Coordinates = new HexCoordinates(3, -1) });
-        map.Add(new BoardHex(neutral) { Coordinates = new HexCoordinates(3, 0) });
+        map.Add(new BoardHex(neutralStar) { Coordinates = new HexCoordinates(3, 0) });
 
         // Italy
         if (playerCount > 5)
@@ -111,9 +159,20 @@ public class EuropeMapGenerator : BaseMapGenerator
         map.Add(new BoardHex(science) { Coordinates = new HexCoordinates(1, 1) });
         map.Add(new BoardHex(capital) { Coordinates = new HexCoordinates(1, 2) });
         map.Add(new BoardHex(star) { Coordinates = new HexCoordinates(0, 3) });
-        map.Add(new BoardHex(sea) { Coordinates = new HexCoordinates(1, 3) });
         map.Add(new BoardHex(sea) { Coordinates = new HexCoordinates(2, 1) });
-
+        map.Add(new BoardHex(sea) { Coordinates = new HexCoordinates(1, 3) });
+        map.Add(new BoardHex()
+        {
+            Coordinates = new HexCoordinates(0, 2),
+            HyperlaneConnections = [
+                new HyperlaneConnection(HexDirection.North, HexDirection.SouthWest),
+                new HyperlaneConnection(HexDirection.NorthEast, HexDirection.SouthWest),
+                new HyperlaneConnection(HexDirection.SouthEast, HexDirection.SouthWest),
+                ]
+        });
+        map.Add(new BoardHex() { Coordinates = new HexCoordinates(-1, 4), HyperlaneConnections = [new HyperlaneConnection(HexDirection.North, HexDirection.SouthEast)] });
+        map.Add(new BoardHex() { Coordinates = new HexCoordinates(0, 4), HyperlaneConnections = [new HyperlaneConnection(HexDirection.NorthWest, HexDirection.NorthEast)] });
+        map.Add(new BoardHex() { Coordinates = new HexCoordinates(2, 2), HyperlaneConnections = [new HyperlaneConnection(HexDirection.SouthWest, HexDirection.North)] });
 
         return map;
     }
