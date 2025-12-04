f6c694de59df3a2f15ca723b4bb701f2d34ef4c3
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2025-12-04 13:47:37 +0100

    Clean up game syncing

--- GameSyncManager.cs
+++ GameSyncManager.cs
@@ -1,24 +1,9 @@
-using System.Collections.Concurrent;
+using AsyncKeyedLock;
 using Google.Cloud.Firestore;
-using SpaceWarDiscordApp.Database;
 
 namespace SpaceWarDiscordApp;
 
 public class GameSyncManager
 {
-    private readonly ConcurrentDictionary<DocumentReference, SemaphoreSlim> _locksByDocumentReference = new();
-    
-    public SemaphoreSlim GetSemaphoreForGame(Game game)
-    {
-        ArgumentNullException.ThrowIfNull(game.DocumentId);
-        
-        var newSemaphore = new SemaphoreSlim(1, 1);
-        if (!_locksByDocumentReference.TryAdd(game.DocumentId, newSemaphore))
-        {
-            newSemaphore.Dispose();
-        }
-        
-        _locksByDocumentReference.TryGetValue(game.DocumentId, out var semaphore);
-        return semaphore!;
-    }
+    public readonly AsyncKeyedLocker<DocumentReference> Locker = new();
 }
